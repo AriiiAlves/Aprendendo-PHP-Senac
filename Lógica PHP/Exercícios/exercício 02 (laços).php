393ee505219cf6ec0e62b773334bcdff791c8331
Ariel Alves <alvzariel@gmail.com>
2023-10-25 14:48:12 -0300

    Exercício 2 (laços) feito

---
+++ Lógica PHP/Exercícios/exercício 02 (laços).php
@@ -0,0 +1,27 @@
+<?php
+
+function Sequencia($par, $inicio, $fim){
+    for ($i=$inicio; $i <= $fim; $i++){
+        if ($i % 2 == 0 && $par == true){
+            $lista[] = $i;
+        }
+        else if ($i % 2 != 0 && $par == false){
+            $lista[] = $i;
+        }
+    }
+    foreach($lista as $num){
+        echo("| $num |");
+    }
+}
+
+# 1. Usando laços de repetição crie uma sequência numérica de números Pares em linha de 0 - 500
+
+Sequencia(true, 0, 500);
+
+echo("<br><br>");
+
+# 2. Usando laços de repetição crie uma sequência numérica de ímpares em linha de 500 - 0
+
+Sequencia(false, 0, 500);
+
+?>
