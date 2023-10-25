06639bdec51c1795622261a33a977cf9cc4ea118
Ariel Alves <alvzariel@gmail.com>
2023-10-25 14:53:50 -0300

    Update exercício 02 (laços).php

--- Lógica PHP/Exercícios/exercício 02 (laços).php
+++ Lógica PHP/Exercícios/exercício 02 (laços).php
@@ -1,14 +1,27 @@
 <?php
 
 function Sequencia($par, $inicio, $fim){
-    for ($i=$inicio; $i <= $fim; $i++){
-        if ($i % 2 == 0 && $par == true){
-            $lista[] = $i;
-        }
-        else if ($i % 2 != 0 && $par == false){
-            $lista[] = $i;
+    if($inicio < $fim){
+        for ($i=$inicio; $i <= $fim; $i++){
+            if ($i % 2 == 0 && $par == true){
+                $lista[] = $i;
+            }
+            else if ($i % 2 != 0 && $par == false){
+                $lista[] = $i;
+            }
         }
     }
+    else{
+        for ($i=$inicio; $i >= $fim; $i--){
+            if ($i % 2 == 0 && $par == true){
+                $lista[] = $i;
+            }
+            else if ($i % 2 != 0 && $par == false){
+                $lista[] = $i;
+            }
+        }
+    }
+    
     foreach($lista as $num){
         echo("| $num |");
     }
@@ -22,6 +35,6 @@ echo("<br><br>");
 
 # 2. Usando laços de repetição crie uma sequência numérica de ímpares em linha de 500 - 0
 
-Sequencia(false, 0, 500);
+Sequencia(false, 500, 0);
 
 ?>
